b1dfafea02c734a3a31ccc564a48fa75cfbaeb53
Govind Bajaj <meetgovindbajaj@gmail.com>
2022-12-16 15:17:52 +0530

    updates

--- config.js
+++ config.js
@@ -1,4 +1,2 @@
 const dotenv = require("dotenv").config();
 module.exports = dotenv;
-// DATABASE=mongodb+srv://govindbajaj:govi1234@alpha-project.wqgpm.mongodb.net/Blog?retryWrites=true&w=majority
-// SECRET_KEY=hellomynameisgovindbajajandiamengineer
